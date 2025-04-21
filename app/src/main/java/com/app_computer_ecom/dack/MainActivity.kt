05bd10601965aad7f5295bbca903b113c60a33b6
khhoine <ngoanhkhoi978@gmail.com>
2025-04-21 16:41:12 +0700

    Fix padding

--- app/src/main/java/com/app_computer_ecom/dack/MainActivity.kt
+++ app/src/main/java/com/app_computer_ecom/dack/MainActivity.kt
@@ -7,10 +7,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.app_computer_ecom.dack.ui.theme.DACKTheme
 import com.app_computer_ecom.dack.viewmodel.AuthViewModel
 
@@ -23,11 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DACKTheme {
-                 Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                ) { innerPadding ->
-                    AppNavigation(authViewModel, Modifier.padding(innerPadding))
-                }
+                AppNavigation(authViewModel)
             }
         }
     }
