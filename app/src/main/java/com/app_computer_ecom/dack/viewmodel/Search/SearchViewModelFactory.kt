a421d2b3a5b9c63849e1c573c02864bc26ecdc3d
khhoine <ngoanhkhoi978@gmail.com>
2025-04-30 14:00:40 +0700

    Add ProductHistory

---
+++ app/src/main/java/com/app_computer_ecom/dack/viewmodel/Search/SearchViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.app_computer_ecom.dack.viewmodel.Search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.app_computer_ecom.dack.data.dao.ProductHistoryDao
+import com.app_computer_ecom.dack.data.dao.SearchHistoryDao
+
+class SearchViewModelFactory(
+    private val searchHistoryDao: SearchHistoryDao,
+    private val productHistoryDao: ProductHistoryDao
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return SearchViewModel(searchHistoryDao, productHistoryDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
